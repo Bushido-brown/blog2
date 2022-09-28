923ba2c9a9d5e8a2cb01c31f29d53e7d20330445
Bushido-brown <orizut@gmail.com>
2022-09-28 08:44:56 -0700

    Write test for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
+  get 'posts/index'
+  get 'posts/show'
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
