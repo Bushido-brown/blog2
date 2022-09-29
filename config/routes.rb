4ffcecb1d6677cebc41d55069670242738cb4187
Bushido-brown <orizut@gmail.com>
2022-09-29 09:19:30 -0700

    Add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/create'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
+    end
   end
+  get 'users/index'
+  get 'users/show'
   get 'posts/index'
   get 'posts/show'
   root 'users#index'
